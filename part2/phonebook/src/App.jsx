12542b8452bad063fc083d5df6dbf49507204a68
RootReturn0 <1121149171@qq.com>
2023-11-18 06:54:12 +0200

    ex 2.6-2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+const Persons = ({ persons }) => (
+  persons.map(person => (
+    <p key={person.name}>{person.name} {person.number}</p>
+  ))
+)
+
+const Filter = ({ search, handleSearchChange }) => {
+  return (
+    <div>
+      <p>filter shown with <input value={search} onChange={handleSearchChange} /></p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [personToShow, setPersonToShow] = useState(persons)
+  const [search, setSearch] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.find(p => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons([...persons, { name: newName, number: newNumber }])
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    setPersonToShow(persons.filter(p => p.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter search={search} handleSearchChange={handleSearchChange} />
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={personToShow} />
+    </div>
+  )
+}
+
+export default App
