c0dfe3febda362dffd91c94012f7463d0d42b240
RootReturn0 <1121149171@qq.com>
2023-11-18 07:03:25 +0200

    ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Persons = ({ persons }) => (
   persons.map(person => (
@@ -15,16 +15,20 @@ const Filter = ({ search, handleSearchChange }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [personToShow, setPersonToShow] = useState(persons)
   const [search, setSearch] = useState('')
+
+  const initDataHook = () => {
+    console.log('effect')
+    const url = 'http://localhost:3001/persons'
+    fetch(url)
+      .then(response => response.json())
+      .then(data => { setPersons(data), setPersonToShow(data) })
+  }
+  useEffect(initDataHook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
