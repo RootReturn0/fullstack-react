82702b1d27fd146ae032788b93f84f616d285a27
RootReturn0 <1121149171@qq.com>
2023-11-19 02:16:03 +0200

    ex 3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+const app = express()
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 
 let persons = [
