a8799d4fc9c952ca4aaf0ef80e99fab4d809f756
RootReturn0 <1121149171@qq.com>
2023-11-19 05:29:04 +0200

    ex 3.15-3.18

--- part3/phonebook-backend-database/index.js
+++ part3/phonebook-backend-database/index.js
@@ -14,8 +14,8 @@ app.use(cors())
 
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        const info = `<p>Phonebook has info for ${persons.length} people</p>`
+    Person.countDocuments({}).then(count => {
+        const info = `<p>Phonebook has info for ${count} people</p>`
         const date = `<p>${new Date()}</p>`
         response.send(info + date)
     })
@@ -34,20 +34,31 @@ app.get('/api/persons/:id', (request, response) => {
         } else {
             response.status(404).end()
         }
-    })
+    }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            Person.findByIdAndDelete(request.params.id).then(() => {
-                response.status(204).end()
-            })
-        } else {
-            response.status(404).end()
-        }
-      })
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -57,7 +68,7 @@ app.post('/api/persons', (request, response) => {
             error: 'name or number missing'
         })
     }
-    
+
     Person.find({ name: body.name }).then(persons => {
         console.log(persons)
         if (persons.length > 0) {
@@ -75,6 +86,24 @@ app.post('/api/persons', (request, response) => {
         }
     })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(unknownEndpoint)
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
