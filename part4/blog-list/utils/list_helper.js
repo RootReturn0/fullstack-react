90af797e2ac3dc78bd74831713727646f29dbe5e
RootReturn0 <1121149171@qq.com>
2023-12-14 04:43:36 +0200

    ex 4.3-4.7

---
+++ part4/blog-list/utils/list_helper.js
@@ -0,0 +1,40 @@
+const totalLikes = (blogs) => {
+    return blogs
+        .map(blog => blog.likes)
+        .reduce((a, b) => a + b, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs
+        .reduce((a, b) => a.likes > b.likes ? a : b)
+}
+
+const mostBlogs = (blogs) => {
+    const authorCount = blogs.reduce((count, blog) => {
+        count[blog.author] = (count[blog.author] || 0) + 1;
+        return count;
+    }, {});
+
+    const maxAuthor = Object.keys(authorCount).reduce((a, b) => {
+        return authorCount[a] > authorCount[b] ? a : b;
+    });
+
+    return {author:maxAuthor, blogs: authorCount[maxAuthor]};
+}
+
+const mostLikes = (blogs) => {
+    const authorLikes = blogs.reduce((count, blog) => {
+        count[blog.author] = (count[blog.author] || 0) + blog.likes;
+        return count;
+    }, {});
+
+    const maxAuthor = Object.keys(authorLikes).reduce((a, b) => {
+        return authorLikes[a] > authorLikes[b] ? a : b;
+    });
+
+    return {author:maxAuthor, likes: authorLikes[maxAuthor]};
+}
+
+module.exports = {
+    totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
